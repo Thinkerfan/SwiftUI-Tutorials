80b0be3eb879167896471caed16cc0d71e0f5898
thinker <731666148@qq.com>
2019-09-15 07:35:02 +0800

    section 4

---
+++ SwiftUI Essentials/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  SwiftUI Essentials
+//
+//  Created by cfans on 2019/9/15.
+//  Copyright © 2019 cfans. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .resizable()
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+            .aspectRatio(contentMode: .fit)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
