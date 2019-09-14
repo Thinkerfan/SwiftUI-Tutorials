597469f91735f355344831a4f2572a44e31392d3
thinker <731666148@qq.com>
2019-09-14 23:09:29 +0800

    Project structure

---
+++ Framework Integration/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Framework Integration
+//
+//  Created by cfans on 2019/9/14.
+//  Copyright © 2019 cfans. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Essentials/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI Essentials
+//
+//  Created by cfans on 2019/9/14.
+//  Copyright © 2019 cfans. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello SwiftUI")
+            .font(.title)
+            .foregroundColor(Color.green)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
