d50e9f23ca49c5e2b475e0ac6e65c91c52bee7cc
thinker <731666148@qq.com>
2019-10-05 23:09:54 +0800

    Drawing and Animation section4

---
+++ Drawing and Animation/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,26 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Drawing and Animation
+//
+//  Created by cfans on 2019/10/5.
+//  Copyright © 2019 cfans. All rights reserved.
+//
+
+import SwiftUI
+
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 5))
+    }
+}
